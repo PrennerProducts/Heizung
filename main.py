8c401c169398c9d95a44527a1e9b1f69631e674a
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2022-01-15 18:33:29 +0100

    Regelintervall geändert

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #####################################################################################################
 # Mischer Motor Steuerung
-REGELINTERVALL = 10 # nur alle 10 Sekunden, soll der Mischermotor angesteuert werden 
+REGELINTERVALL = 30 # nur alle % Sekunden, soll der Mischermotor angesteuert werden 
 MAX_REGELDIFFERENZ = 10.0 # Nur maximal 10 Grad Regelabweichung werden beruecksichtigt, damit der Regler nicht zu agressiv regelt
 HYSTERESE_VORLAUFTEMPERATUR = 0.8 # Temperaturbereich, innerhalb dem nicht nachgeregelt wird
 STELLZEIT_PRO_KELVIN_TEMP_DIFF = 0.5; # Wie viele Sekunden soll der Mischermotor pro Kelvin Temperaturabweichung und Regelintervall angesteuert werden?
